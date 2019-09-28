92de527acaccd1079b72a0123bcb807d64ac8584
dancorrea86 <danielmacielcorrea@gmail.com>
2019-09-28 15:47:42 -0300

    arquivo search2

---
+++ searchFile2.py
@@ -0,0 +1,23 @@
+# coding: UTF-8
+import os
+import re
+
+count = 2
+
+empresas = ['1000', '1001', '1002']
+ano = '2019'
+mes = '06.2019'
+
+my_dir = "/home/daniel/Programação/Projects/searchforfileinpasteswithpython/c-test-files/"
+
+for dir, sub, files in os.walk(my_dir):
+	if dir == (my_dir  + empresas[count] + '/FISCAL/' + ano + '/' + mes + '/' + 'Declaração'):
+		print (empresas[count])
+		print (files)	
+		print ('*' * 60)
+		count -= 1
+
+
+
+#/home/daniel/Programação/Projects/searchforfileinpasteswithpython/c-test-files/1000/FISCAL/2019/06/2019/Declaração
+#/home/daniel/Programação/Projects/searchforfileinpasteswithpython/c-test-files/1000/FISCAL/2019/01.2019/Declaração
